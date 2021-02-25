c64d8a4a4428c5534bf61765f738591764500385
Alex Ivanov <alexivanovv4@gmail.com>
2021-02-25 10:01:59 +0200

    Add

---
+++ MathFundamentalsInProgramming/Converter/Converter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write($"Enter first number type: ");
+            string firstNumberType = Console.ReadLine();
+            Console.Write($"Enter first number: ");
+            string firstNumber = Console.ReadLine();
+            Console.Write($"Enter second number type: ");
+            string secondNumberType = Console.ReadLine();
+            Console.Write($"Enter second number: ");
+            string secondNumber = Console.ReadLine();
+            Console.Write($"Enter action: ");
+            string action = Console.ReadLine();
+        }
+    }
+}
