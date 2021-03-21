4eb595467b8d26be4fa2cdb0ef1b20eb0a444277
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-03-21 16:24:10 +0200

    Add

---
+++ OOP/190321/1ex/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Vavedi obsht broi resultati: ");
+                int n = int.Parse(Console.ReadLine());
+                NValidation(n);
+                List<int> results = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Vavedi resultat: ");
+                    int result = int.Parse(Console.ReadLine());
+                    ResultValidation(result);
+
+                    results.Add(result);
+                }
+
+                int counter = 1;
+                foreach (var result in results.OrderByDescending(x => x))
+                {
+                    if (counter == 11)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{counter} mqsto- {result} tochki");
+                        counter++;
+                    }
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
+        }
+
+        private static void NValidation(int n)
+        {
+            if (n <= 10 || n > 130)
+            {
+                throw new ArgumentException("Invalid num!");
+            }           
+        }
+        private static void ResultValidation(int result)
+        {
+            if (result < 0 || result > 100)
+            {
+                throw new ArgumentException("Invalid result!");
+            }
+        }
+    }
+}
---
+++ OOP/190321/2ex/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace _2ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(11, 130);
+
+           
+            for (int i = 0; i < n; i++)
+            {
+                int result = rnd.Next(0, 100);
+
+                StreamWriter output = new StreamWriter("file.txt", false, Encoding.GetEncoding("Unicode"));
+                output.Write(result + " ");
+                
+            }
+            
+            List<int> results = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                StreamReader inputFile = new StreamReader("file.txt", Encoding.GetEncoding("Unicode"));
+
+                string input = inputFile.ReadLine();
+                    inputFile.Close();
+                    results = input
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+                   
+                
+            }
+            int counter = 1;
+            foreach (var result in results.OrderByDescending(x => x))
+            {
+                if (counter == 11)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{counter} mqsto- {result} tochki");
+                    counter++;
+                }
+            }
+            //using (FileStream fs = new FileStream(cDirs + @"\Results.txt", FileMode.Truncate))
+            //{
+            //}
+        }
+    }
+}
