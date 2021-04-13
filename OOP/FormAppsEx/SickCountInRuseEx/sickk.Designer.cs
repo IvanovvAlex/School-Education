166aff58bd846034abd67bff0bfe2d29e9db8909
Alex Ivanov <alexivanovv4@gmail.com>
2021-04-13 23:42:25 +0300

    Add

---
+++ OOP/FormAppsEx/SickCountInRuseEx/sickk.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace SickCountInRuseEx
+{
+    partial class sickk
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Болни в Русе през седмицата:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-60, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(255, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(80, 19);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "низходящ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(255, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(79, 19);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "възходящ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 7;
+            // 
+            // sickk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 391);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "sickk";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.sickk_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OOP/FormAppsEx/SickCountInRuseEx/sickk.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SickCountInRuseEx
+{
+    public partial class sickk : Form
+    {
+        static List<TextBox> bolni = new List<TextBox>();
+        public sickk()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form.ActiveForm.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> info = new List<int>();
+            try
+            {
+                foreach (var item in bolni)
+                {
+                    if (int.Parse(item.Text) < 0)
+                    {
+                        throw new Exception();
+                    }
+                    info.Add(int.Parse(item.Text));
+                }
+            }
+            catch (Exception)
+            {
+                CleanAll();
+                MessageBox.Show("Invalid input!");
+                
+            }
+            if (radioButton1.Checked == true)
+            {
+                label3.Text = (string.Join(" - ", info.OrderByDescending(x => x)));
+                //BUBBLE SORTING
+                //bubbleSortDescending(info, info.Count);
+                //label3.Text = (string.Join(" - ", info));
+
+            }
+            if (radioButton2.Checked == true)
+            {
+                label3.Text = (string.Join(" - ", info.OrderBy(x => x)));
+                //BUBBLE SORTING
+                //bubbleSort(info, info.Count);
+                //label3.Text = (string.Join(" - ", info));
+            }
+            if(radioButton1.Checked == false && radioButton2.Checked == false)
+            { MessageBox.Show("There is no chosen option. Try again..."); }
+        }
+
+        private void sickk_Load(object sender, EventArgs e)
+        {
+            int y = 40;
+            for (int n = 0; n < 7; n++)
+            {
+                var txtBox = new TextBox();               
+                txtBox.Location = new Point(30, y);                
+                y += 30;
+                Controls.Add(txtBox);              
+                bolni.Add(txtBox);                
+            } 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CleanAll();
+            //label3.Text = "";
+            //radioButton1.Checked = false;
+            //radioButton2.Checked = false;
+            //for (int i = 0; i < bolni.Count; i++)
+            //{
+            //    bolni[i].Text = "";
+            //}
+        }
+        private void CleanAll()
+        {
+            label3.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            for (int i = 0; i < bolni.Count; i++)
+            {
+                bolni[i].Text = "";
+            }
+        }
+        static void bubbleSort(List<int> list, int listsCount)
+        {            
+            bool swapped;
+            for (int i = 0; i < listsCount - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < listsCount - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {                        
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                        swapped = true;
+                    }
+                }              
+                if (swapped == false)
+                    break;
+            }
+        }
+        static void bubbleSortDescending(List<int> list, int listsCount)
+        {
+            bool swapped;
+            for (int i = 0; i < listsCount - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < listsCount - i - 1; j++)
+                {
+                    if (list[j] < list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                    break;
+            }
+        }
+    }
+}
