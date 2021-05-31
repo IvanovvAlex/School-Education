73e789ee4479c77b703a364d517605cd6b339a81
Alex Ivanov <alexivanovv4@gmail.com>
2021-05-31 09:25:21 +0300

    Edit

--- OOP/25052021/str59-zad2/Form1.Designer.cs
+++ OOP/25052021/str59-zad2/Form1.Designer.cs
@@ -32,68 +32,73 @@ namespace str59_zad2
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.nLabel = new System.Windows.Forms.Label();
+            this.mLabel = new System.Windows.Forms.Label();
+            this.titleLable = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(70, 73);
+            this.textBox1.Location = new System.Drawing.Point(59, 49);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(47, 23);
+            this.textBox1.Size = new System.Drawing.Size(29, 23);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(175, 72);
+            this.textBox2.Location = new System.Drawing.Point(186, 49);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(63, 23);
+            this.textBox2.Size = new System.Drawing.Size(29, 23);
             this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(334, 72);
+            this.button1.Location = new System.Drawing.Point(295, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "CREATE";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox3
+            // nLabel
             // 
-            this.textBox3.Location = new System.Drawing.Point(255, 113);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
-            this.textBox3.TabIndex = 3;
+            this.nLabel.AutoSize = true;
+            this.nLabel.Location = new System.Drawing.Point(37, 52);
+            this.nLabel.Name = "nLabel";
+            this.nLabel.Size = new System.Drawing.Size(16, 15);
+            this.nLabel.TabIndex = 3;
+            this.nLabel.Text = "N";
             // 
-            // button2
+            // mLabel
             // 
-            this.button2.Location = new System.Drawing.Point(381, 112);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(28, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.mLabel.AutoSize = true;
+            this.mLabel.Location = new System.Drawing.Point(164, 52);
+            this.mLabel.Name = "mLabel";
+            this.mLabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.mLabel.Size = new System.Drawing.Size(18, 15);
+            this.mLabel.TabIndex = 4;
+            this.mLabel.Text = "M";
             // 
-            // button3
+            // titleLable
             // 
-            this.button3.Location = new System.Drawing.Point(334, 161);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.titleLable.AutoSize = true;
+            this.titleLable.Location = new System.Drawing.Point(152, 18);
+            this.titleLable.Name = "titleLable";
+            this.titleLable.Size = new System.Drawing.Size(91, 15);
+            this.titleLable.TabIndex = 5;
+            this.titleLable.Text = "Dynamic Matrix";
+            this.titleLable.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 393);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.titleLable);
+            this.Controls.Add(this.mLabel);
+            this.Controls.Add(this.nLabel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -110,9 +115,9 @@ namespace str59_zad2
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label nLabel;
+        private System.Windows.Forms.Label mLabel;
+        private System.Windows.Forms.Label titleLable;
     }
 }
 
--- OOP/25052021/str59-zad2/Form1.cs
+++ OOP/25052021/str59-zad2/Form1.cs
@@ -12,6 +12,8 @@ namespace str59_zad2
 {
     public partial class Form1 : Form
     {
+     
+
         public Form1()
         {
             InitializeComponent();
@@ -19,56 +21,70 @@ namespace str59_zad2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Point merlokacionin = this.label.Location;// This take the location in form . We have added a label with no text just to take the location and under it to add fields.
-
-            int a = merlokacionin.X + 80;//The coordinate X of the label we give to the int variable a
-
-            int b = merlokacionin.Y;//The coordinate Y
-
-            if ((textBox1.Text == "") || (textBox2.Text == ""))
-                MessageBox.Show(" To create a Matrices you need to give up the numbers of columns and rows.,"opps",MessageBoxButtons.OK,MessageBoxIcon.Error);
-            else
-            {
-                n = Convert.ToInt32(textBox1.Text);// Take the value N -Rows
-                m = Convert.ToInt32(textBox2.Text);// Take the value M-Columns
-            }
-            int i, j;
-
-            int[,] Matrica;
-
-            Matrica = new int[n, m];//
-
-            for (i = 0; i < n; i++)
-            {
-                a = merlokacionin.X + 10; // fields will be at coordinate X      //                                         in 10
-                point at right from label1.
-
-                                b = b + 30;// the coordinate of b is changed with adding in 30 points , so the //field will be added in  30 points under the label1.
-
-                for (j = 0; j < m; j++)
-                {
-
-                    textbox[i, j] = new TextBox();    //  it create the new //textbox field, the creating fields will continue while as we have the //columns and rows.
-
-                    string emriTextBox = "A" + Convert.ToString(i);//We set the name for textbox
-
-                    this.textbox[i, j].Name = emriTextBox; //give name to textbox
-
-                    a = a + 25;// change the coordinate of X and every 25 points will be the next field                   
-                    textbox[i, j].Width = 25;
-                    textbox[i, j].Height = 25;
-                    textbox[i, j].Location = new Point(a, b + 30);//Now the field take the location where it will be pasted.
-
-                    this.Controls.Add(textbox[i, j]);
-
-                }
-
-            }
+            
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(textBox1.Text);
+                int m = int.Parse(textBox2.Text);
+                int counter = 0;
+                TextBox[] MatrixNodes = new TextBox[n * m];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        var tb = new TextBox();
+                        Random r = new Random();
+                        int num = r.Next(1, 1000);
+                        MatrixNodes[counter] = tb;
+                        tb.Name = "Node_" + MatrixNodes[counter];
+                        tb.Text = num.ToString();
+                        tb.Location = new Point(50 + (j * 150), 150 + (i * 50));
+                        tb.Visible = true;
+                        this.Controls.Add(tb);
+                        counter++;
+                    }
+
+                    counter = 0;
+                }
+                //if (int.Parse(textBox1.Text) > 0 && int.Parse(textBox2.Text) > 0)
+                //{
+                //    int y = 225;
+                //    for (int n = 0; n < 5; n++)
+                //    {
+                //        var txtBox = new TextBox();                        
+                //        var txtBox2 = new TextBox();
+                //        txtBox.Location = new Point(50, y);
+                //        txtBox2.Location = new Point(50, y + 25);
+                //        y += 50;
+                //        Controls.Add(txtBox);
+                //        Controls.Add(txtBox2);
+                //        list.Add(txtBox);
+                //        list.Add(txtBox2);
+                //    }
+           
+               
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Invalid input");
+            }
+            
         }
     }
 }
