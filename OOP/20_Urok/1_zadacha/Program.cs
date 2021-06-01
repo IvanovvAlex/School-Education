95905cf50615e6fbc62e0c7b366bec49fa34e5fe
Alex Ivanov <alexivanovv4@gmail.com>
2021-06-01 23:50:57 +0300

    Add

---
+++ OOP/20_Urok/1_zadacha/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace _1_zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class SetByte
+    {
+        public byte[] elements;
+        public int size;
+
+        public SetByte()
+        {
+            this.size = 0;
+            this.elements = new byte[256];
+        }
+        public SetByte(byte[] arr, int length)
+        {
+            this.size = length;
+            this.elements = new byte[256];
+            if (this.size != 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    this.elements[i] = arr[i];
+                }
+            }
+            Array.Sort(this.elements, 0, length);
+        }
+        public SetByte(SetByte setByte)
+        {
+            this.size = setByte.size;
+            this.elements = new byte[256];
+            if (size != 0)
+            {
+                for (int i = 0; i < setByte.size; i++)
+                {
+                    this.elements[i] = setByte.elements[i];
+                }
+            }
+        }
+        public SetByte Union(SetByte b)
+        {
+            SetByte c = new SetByte();
+            int n1 = 0;
+            int n2 = 0;
+            int n = -1;
+            int i;
+
+            while (n1 < this.size && n2 < b.size)
+            {
+                if (this.elements[n1] <= b.elements[n2]) 
+                {
+                    c.elements[++n] = this.elements[n1++];
+                }
+                else
+                {
+                    c.elements[++n] = b.elements[n2++];
+                }
+                if (this.elements[n1] == b.elements[n2])
+                {
+                    n2++;
+                }
+            }
+            if (n1 == this.size)
+            {
+                for ( i = n2; i < b.size; i++)
+                {
+                    c.elements[++n] = b.elements[i];
+                }
+            }
+            else
+            {
+                for ( i = n1; i < this.size; i++)
+                {
+                    c.elements[++n] = this.elements[i];
+                }
+            }
+            c.size = (byte)(n + 1);
+            return c;
+        }
+        public SetByte Intersection(SetByte b)
+        {
+            SetByte c = new SetByte();
+            int n1 = 0;
+            int n2 = 0;
+            int n = -1;
+            int i;
+            while (n1 < this.size && n2 < b.size)
+            {
+                if (this.elements[n1] < b.elements[n2])
+                {
+                    n1++;
+                }
+                else if (this.elements[n1] > b.elements[n2])
+                {
+                    n2++;
+                }
+                else
+                {
+                    c.elements[++n] = this.elements[n1++];
+                    n2++;
+                }
+            }
+            c.size = (byte)(n + 1);
+            return c;
+        }
+        public void Print()
+        {
+            Console.Write("{ ");
+            if (this.size != 0)
+            {
+                for (int i = 0; i < this.size; i++)
+                {
+                    Console.Write("{0}, ", this.elements[i]);
+                }
+                Console.Write("{0} ", this.elements[size-1]);
+            }
+            Console.WriteLine("}");
+        }
+    }
+}
--- Projects/BooksStoreForms/BooksStoreForms/Form1.cs
+++ Projects/BooksStoreForms/BooksStoreForms/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,20 +13,54 @@ namespace BooksStoreForms
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-3U20D3B;Initial Catalog=BookStoreDB;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        //ID variable used in Updating and Deleting Record  
+        int ID = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) // readButton
         {
-
+            if (AuthorsRadioButton.Checked == true)
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("select * from Authors", con);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            else if (GenresRadioButton.Checked == true)
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("select * from Genres", con);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            else if (BooksRadioButton.Checked == true)
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("select * from Books", con);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please select table to read");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-           
+                       
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -91,22 +126,150 @@ namespace BooksStoreForms
 
         private void btn_create_Click(object sender, EventArgs e)
         {
+            if (AuthorsRadioButton.Checked == true)
+            {
+                if (txt_ID.Text != "" && txt_Name.Text != "")
+                {
+                    cmd = new SqlCommand("insert into Authors(AuthorsID,AuthorsName) values(@id,@name)", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txt_ID.Text));
+                    cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Record Inserted Successfully");
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Please Provide Details!");
+                }
+                                         
+            }
+            else if (GenresRadioButton.Checked == true)
+            {
+                if (txt_ID.Text != "" && txt_Name.Text != "")
+                {
+                    cmd = new SqlCommand("insert into Genres(GenresID,GenresName) values(@id,@name)", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txt_ID.Text));
+                    cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Record Inserted Successfully");
 
+                }
+                else
+                {
+                    MessageBox.Show("Please Provide Details!");
+                }
+            }
+            else if (BooksRadioButton.Checked == true)
+            {
+                if (txt_ID.Text != "" && txt_Name.Text != "" && txt_ISBN.Text != "" && txt_GenreID.Text != "")
+                {
+                    cmd = new SqlCommand("insert into Books(BooksID,BooksName,BooksISBN,GenresID) values(@id,@name,@isbn, @genreId)", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txt_ID.Text));
+                    cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                    cmd.Parameters.AddWithValue("@isbn", int.Parse(txt_ISBN.Text));
+                    cmd.Parameters.AddWithValue("@genreId", int.Parse(txt_GenreID.Text));
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Record Inserted Successfully");
+
+                }
+                else
+                {
+                    MessageBox.Show("Please Provide Details!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please choose table!");
+            }
         }
 
         private void btn_update_Click(object sender, EventArgs e)
         {
+            if (AuthorsRadioButton.Checked == true)
+            {
+                if (txt_Name.Text != "" && txt_ID.Text != "")
+                {
+                    cmd = new SqlCommand("update Authors set AuthorsName=@name where AuthorsID=@id", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txt_ID.Text));
+                    cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record Updated Successfully");
+                    con.Close();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Please Select Record to Update");
+                }
+            }
+            else if (GenresRadioButton.Checked == true)
+            {
+                if (txt_Name.Text != "" && txt_ID.Text != "")
+                {
+                    cmd = new SqlCommand("update Genres set GenresName=@name where GenresID=@id", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txt_ID.Text));
+                    cmd.Parameters.AddWithValue("@name", txt_Name.Text);                    
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record Updated Successfully");
+                    con.Close();
+                   
+                }
+                else
+                {
+                    MessageBox.Show("Please Select Record to Update");
+                }
+            }
+            else if (BooksRadioButton.Checked == true)
+            {
+                if (txt_Name.Text != "" && txt_ID.Text != "")
+                {
+                    cmd = new SqlCommand("update Genres set GenresName=@name where GenresID=@id", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txt_ID.Text));
+                    cmd.Parameters.AddWithValue("@name", txt_Name.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record Updated Successfully");
+                    con.Close();
 
+                }
+                else
+                {
+                    MessageBox.Show("Please Select Record to Update");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please choose table!");
+            }
         }
 
         private void btn_delete_Click(object sender, EventArgs e)
         {
+            if (AuthorsRadioButton.Checked == true)
+            {
 
+            }
+            else if (GenresRadioButton.Checked == true)
+            {
+
+            }
+            else
+            {
+
+            }
         }
 
         private void btn_exit_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
