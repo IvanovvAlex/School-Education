90dfe6f91e76798ea9db8005f37aba4c85dcdb22
Alex Ivanov <alexivanovv4@gmail.com>
2022-01-24 16:36:03 +0200

    Add

---
+++ WebDevelopment-RU/BasicHTML-CSSWebPage/script.js
@@ -0,0 +1,12 @@
+function ChangeValues(){
+    debugger;
+    let ime = document.getElementById("ime").value;
+    let familiq = document.getElementById("fam").value;
+   
+    let oldName = ime;
+    let oldFam = familiq; 
+
+    document.getElementById("ime").value = oldFam;
+    document.getElementById("fam").value = oldName;
+    document.querySelector()
+}
